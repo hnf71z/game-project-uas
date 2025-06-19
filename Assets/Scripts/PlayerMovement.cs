f87b3c085e359a3f451246719fffe9c54bf5eefe
jonathanordrick <edrawijaya77@gmail.com>
2025-06-19 16:04:54 +0700

    Implement player movement and attack mechanics with jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,95 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 5f;
+    public float jumpForce = 5f;
     public Rigidbody2D rb;
     public Animator anim;
     public int facingDirection = 1;
+    public PlayerAttack playerAttack;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
+    public int maxJumps = 2;
+    private int jumpCount = 0;
 
-    // Update is called once per frame
+private bool isGrounded;
+    private bool jumpPressed = false;
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
 
-        if(horizontal > 0 && transform.localScale.x < 0 ||
-            horizontal < 0 && transform.localScale.x > 0)
-            {
-                Flip();
-            }
+        // Cek apakah menyentuh tanah
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
+        // Reset jumlah lompatan saat menyentuh tanah
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+
+        // Jika Space ditekan dan masih punya jatah lompatan
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
+        {
+            jumpPressed = true;
+            jumpCount++;
+        }
+
+        // Input loncat hanya jika menyentuh tanah
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jumpPressed = true;
+        }
+
+        // Serangan
+        if (Input.GetButtonDown("Attack"))
+        {
+            playerAttack.Attack();
+        }
+
+        // Animasi jalan
         anim.SetFloat("Horizontal", Mathf.Abs(horizontal));
 
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        // Flip arah
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+    }
 
-        if(Input.GetKeyDown(KeyCode.Space))
-            rb.velocity = new Vector2(rb.velocity.x, speed);
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
 
+        // Gerak horizontal
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // Eksekusi loncat
+        if (jumpPressed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpPressed = false; // reset
+        }
     }
 
     void Flip()
     {
         facingDirection *= -1;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        transform.localScale = new Vector3(
+            transform.localScale.x * -1,
+            transform.localScale.y,
+            transform.localScale.z
+        );
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
